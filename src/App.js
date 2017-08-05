2274958109932438c2a974f077e4128220609e5f
patw <patw.hi@gmail.com>
2017-08-05 12:17:12 +0800

    Using SCSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <div className={styles['App-header']}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className={styles['App-intro']}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
