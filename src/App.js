4da442430c08222cb0c07420e153c504c75c02c0
patw <patw.hi@gmail.com>
2017-08-05 14:44:54 +0800

    Move fake jobs data into App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import styles from './App.scss';
 import JobList from './components/JobList/JobList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.jobs = [{
+      id: 3,
+      title: 'Frontend Developer',
+      company: 'Faceb00k',
+    }, {
+      id: 2,
+      title: 'Backend Developer',
+      company: 'Goog1e',
+    }, {
+      id: 1,
+      title: 'UI Designer',
+      company: '4irBnB',
+    }];
+  }
+
   render() {
     return (
       <div className={styles.App}>
@@ -11,7 +29,7 @@ class App extends Component {
           <img src={logo} className={styles['App-logo']} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <JobList />
+        <JobList jobs={this.jobs} />
       </div>
     );
   }
--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Job from './Job';
 
 export default class JobList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.jobs = [{
-      id: 3,
-      title: 'Frontend Developer',
-      company: 'Faceb00k',
-    }, {
-      id: 2,
-      title: 'Backend Developer',
-      company: 'Goog1e',
-    }, {
-      id: 1,
-      title: 'UI Designer',
-      company: '4irBnB',
-    }]
-  }
+  static propTypes = {
+    jobs: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      company: PropTypes.string,
+    })),
+  };
 
   render() {
     return (
       <div>
-        {this.jobs.map(job => {
+        {this.props.jobs.map(job => {
           return <Job key={job.id} title={job.title} company={job.company} />;
         })}
       </div>
